a7b4a5fc48dabd988723501679670243ce6b5295
Jonas Johansson <jns.johansson@gmail.com>
2024-06-09 13:46:02 +0200

    x

--- js/script.js
+++ js/script.js
@@ -32,7 +32,12 @@ let p2_score = localStorage.getItem("p2_score") || 0;
 document.documentElement.style.setProperty("--p1_score", `"${p1_score}"`);
 document.documentElement.style.setProperty("--p2_score", `"${p2_score}"`);
 
-const pid = localStorage.getItem("pid") || prompt("Which player ID are you (enter 1 or 2)?");
+let pid = localStorage.getItem("pid");
+
+if (typeof pid !== "number"){
+    pid = prompt("Vilket spelar-ID är du (ange 1 eller 2")
+}
+
 localStorage.setItem("pid", pid);
 console.log(`I am player ${pid}`);
 document.body.setAttribute("data-pid", pid);

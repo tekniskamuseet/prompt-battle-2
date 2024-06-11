9685b2f3c2ffa1497186af89df90dbfaca812efc
Jonas Johansson <jns.johansson@gmail.com>
2024-06-11 02:11:55 +0200

    x

--- js/script.js
+++ js/script.js
@@ -1,36 +1,54 @@
-const getElement = (id) => document.querySelector(id);
+const elements = (() => {
+    const ids = [
+        "step-1",
+        "step-2",
+        "step-3",
+        "playerScore",
+        "playerName",
+        "timer",
+        "imageResult",
+        "nameInput",
+        "resetClientsButton",
+        "nameSubmitButton",
+        "promptInput",
+        "promptText",
+        "promptSubmitButton",
+        "generatedImage",
+        "tryAgainButton",
+        "restartButton",
+        "errorMessage",
+        "scoreIncreaseAudio",
+        "generatingImageAudio",
+    ];
+    const query = (id) => document.querySelector(`#${id}`);
+    return ids.reduce((acc, id) => ({ ...acc, [id]: query(id) }), {});
+})();
 
-const elements = {
-    step1: getElement("#step-1"),
-    step2: getElement("#step-2"),
-    step3: getElement("#step-3"),
-    playerScore: getElement("#playerScore"),
-    playerName: getElement("#playerName"),
-    timer: getElement("#timer"),
-    imageResult: getElement("#imageResult"),
-    nameInput: getElement("#nameInput"),
-    resetClientsButton: getElement("#resetClientsButton"),
-    nameSubmitButton: getElement("#nameSubmitButton"),
-    promptInput: getElement("#promptInput"),
-    promptText: getElement("#promptText"),
-    promptSubmitButton: getElement("#promptSubmitButton"),
-    generatedImage: getElement("#generatedImage"),
-    tryAgainButton: getElement("#tryAgainButton"),
-    restartButton: getElement("#restartButton"),
-    errorMessage: getElement("#errorMessage"),
-    scoreIncreaseAudio: getElement("#scoreIncreaseAudio"),
-    generatingImageAudio: getElement("#generatingImageAudio"),
-};
+const {
+    playerName,
+    nameInput,
+    promptInput,
+    promptSubmitButton,
+    timer,
+    errorMessage,
+    generatedImage,
+    nameSubmitButton,
+    tryAgainButton,
+    restartButton,
+} = elements;
 
-let p1_color = localStorage.getItem("p1_color") || "red";
-let p2_color = localStorage.getItem("p2_color") || "blue";
-document.documentElement.style.setProperty("--p1_color", p1_color);
-document.documentElement.style.setProperty("--p2_color", p2_color);
+const setProperty = (property, value) =>
+    document.body.style.setProperty(property, value);
+
+const p1_color = localStorage.getItem("p1_color") || "red";
+const p2_color = localStorage.getItem("p2_color") || "blue";
+setProperty("--p1_color", p1_color);
+setProperty("--p2_color", p2_color);
 
 let p1_score = localStorage.getItem("p1_score") || 0;
 let p2_score = localStorage.getItem("p2_score") || 0;
-document.documentElement.style.setProperty("--p1_score", `"${p1_score}"`);
-document.documentElement.style.setProperty("--p2_score", `"${p2_score}"`);
+setProperty("--p1_score", `"${p1_score}"`);
+setProperty("--p2_score", `"${p2_score}"`);
 
 let pid = localStorage.getItem("pid");
 
@@ -38,31 +56,27 @@ while (pid !== "1" && pid !== "2") {
     pid = prompt("Vilket spelar-ID är du (ange 1 eller 2)");
 }
 
-localStorage.setItem("pid", parseInt(pid));
+localStorage.setItem("pid", pid);
 document.body.setAttribute("data-pid", pid);
-document.documentElement.style.setProperty("--pid", `"${pid}"`);
-elements.playerName.textContent = `Spelare #${pid}:`;
+setProperty("--pid", `"${pid}"`);
+playerName.textContent = `Spelare #${pid}:`;
 
-let socket = new WebSocket("wss://prompt-battle-server.glitch.me");
-
-/* Start Game */
+const socket = new WebSocket("wss://prompt-battle-server.glitch.me");
 
 const submitName = () => {
-    playerName = elements.nameInput.value.trim();
-    if (!playerName) {
+    const name = nameInput.value.trim();
+    if (!name) {
         alert("Fyll i ditt namn.");
         return;
     }
-    document.documentElement.style.setProperty("--player_name", `"${playerName}"`);
+    setProperty("--player_name", `"${name}"`);
     document.body.setAttribute("data-step", 2);
-    focusInput(elements.promptInput);
+    focusInput(promptInput);
     startTimer();
 };
 
-/* Timer Logic */
-
 let countdown;
-let duration = 60;
+const duration = 60;
 
 const startTimer = () => {
     let timeLeft = duration;
@@ -78,12 +92,12 @@ const startTimer = () => {
 };
 
 const setTimer = (timeLeft) => {
-    elements.timer.textContent = `Du har ${--timeLeft} sekunder kvar…`;
+    timer.textContent = timeLeft
+        ? `Du har ${timeLeft} sekunder kvar…`
+        : `Tiden är slut!`;
 };
 
 const clearTimer = () => clearInterval(countdown);
-
-/* UX */
 
 const focusInput = (input) => input.focus();
 
@@ -96,35 +110,29 @@ const handleKeyPress = (input, button) => {
     });
 };
 
-/* Reset Logic */
-
-const resetForm = (fullReset = false) => {
+const resetForm = () => {
     document.body.setAttribute("data-step", 2);
     clearTimer();
-    elements.errorMessage.textContent = "";
-    elements.promptInput.value = "";
-    elements.generatedImage.src = "";
-    elements.promptInput.disabled = false;
-    elements.promptSubmitButton.disabled = false;
-    elements.errorMessage.textContent = "";
+    errorMessage.textContent = "";
+    promptInput.textContent = "";
+    generatedImage.src = "";
+    promptInput.disabled = false;
+    promptSubmitButton.disabled = false;
     startTimer();
 };
 
-/* AI Logic */
-
 const submitPrompt = () => {
-    const promptText = elements.promptInput.value.trim();
+    const promptText = promptInput.textContent.trim();
     if (!promptText) {
-        alert("Fyll i din prompt.");
+        // alert("Fyll i din prompt.");
         return;
     }
 
-    elements.timer.textContent = `Bilden genereras…`;
-    generatingImageAudio.play();
+    elements.generatingImageAudio.play();
     elements.promptText.textContent = promptText;
-    elements.promptInput.disabled = true;
-    elements.promptSubmitButton.disabled = true;
-    elements.errorMessage.textContent = "";
+    promptInput.disabled = true;
+    promptSubmitButton.textContent = `Bilden genereras…`;
+    promptSubmitButton.disabled = true;
     clearTimer();
 
     socket.send(
@@ -135,30 +143,25 @@ const submitPrompt = () => {
     );
 };
 
-/* LISTENER */
+nameSubmitButton.addEventListener("click", submitName);
+promptSubmitButton.addEventListener("click", submitPrompt);
+tryAgainButton.addEventListener("click", resetForm);
+restartButton.addEventListener("click", () => location.reload());
 
-elements.nameSubmitButton.addEventListener("click", () => submitName());
-elements.promptSubmitButton.addEventListener("click", () => submitPrompt());
-elements.tryAgainButton.addEventListener("click", () => resetForm());
-elements.restartButton.addEventListener("click", () => location.reload());
+handleKeyPress(nameInput, nameSubmitButton);
+handleKeyPress(promptInput, promptSubmitButton);
 
-handleKeyPress(elements.nameInput, elements.nameSubmitButton);
-handleKeyPress(elements.promptInput, elements.promptSubmitButton);
-
-/* socket logic */
-
-const handleSocketMessage = (event) => {
-    const data = JSON.parse(event.data);
-    const type = data.type;
-    const payload = data.payload;
-    console.log(payload)
-
+socket.addEventListener("open", () =>
+    console.log("WebSocket connection established.")
+);
+socket.addEventListener("message", (event) => {
+    const { type, payload, imageUrl } = JSON.parse(event.data);
     switch (type) {
         case "imageGenerated":
-            elements.generatedImage.src = data.imageUrl;
-            elements.timer.textContent = "";
-            generatingImageAudio.pause();
-            generatingImageAudio.currentTime = 0;
+            generatedImage.src = imageUrl;
+            timer.textContent = "";
+            elements.generatingImageAudio.pause();
+            elements.generatingImageAudio.currentTime = 0;
             document.body.setAttribute("data-step", 3);
             break;
         case "reset":
@@ -166,58 +169,43 @@ const handleSocketMessage = (event) => {
             location.reload();
             break;
         case "updateColor":
-            document.documentElement.style.setProperty(
-                "--p1_color",
-                payload.p1_color
-            );
-            document.documentElement.style.setProperty(
-                "--p2_color",
-                payload.p2_color
-            );
+            setProperty("--p1_color", payload.p1_color);
+            setProperty("--p2_color", payload.p2_color);
             localStorage.setItem("p1_color", payload.p1_color);
             localStorage.setItem("p2_color", payload.p2_color);
             break;
         case "updateScore":
-            document.documentElement.style.setProperty(
-                "--p1_score",
-                `"${payload.p1_score}"`
-            );
-            document.documentElement.style.setProperty(
-                "--p2_score",
-                `"${payload.p2_score}"`
-            );
+            setProperty("--p1_score", `"${payload.p1_score}"`);
+            setProperty("--p2_score", `"${payload.p2_score}"`);
             payload.p1_score = parseInt(payload.p1_score);
             payload.p2_score = parseInt(payload.p2_score);
-            if (pid == 1 && payload.p1_score > p1_score) {
+            if (pid == 1 && payload.p1_score > p1_score)
                 playSound("scoreIncreaseAudio");
-            }
-            if (pid == 2 && payload.p2_score > p2_score) {
+            if (pid == 2 && payload.p2_score > p2_score)
                 playSound("scoreIncreaseAudio");
-            }
             localStorage.setItem("p1_score", payload.p1_score);
             localStorage.setItem("p2_score", payload.p2_score);
             p1_score = payload.p1_score;
             p2_score = payload.p2_score;
             break;
         case "setDuration":
-            duration = payload.duration;
             localStorage.setItem("duration", `"${payload.duration}"`);
             break;
         case "error":
-            elements.errorMessage.textContent = data.message;
-            elements.promptInput.disabled = false;
-            elements.promptSubmitButton.disabled = false;
+            alert(
+                "Oops! Det verkar som att din prompt fick DALL-E att rodna. Städa upp och försök igen!"
+            );
+            promptSubmitButton.textContent =
+                promptSubmitButton.getAttribute("alt");
+            promptInput.disabled = false;
+            promptSubmitButton.disabled = false;
+            startTimer();
             break;
         default:
             console.warn("Unknown message type received:", type);
             break;
     }
-};
-
-socket.addEventListener("open", () => {
-    console.log("WebSocket connection established.");
 });
-socket.addEventListener("message", handleSocketMessage);
 socket.addEventListener("close", () =>
     console.log("WebSocket connection closed.")
 );
@@ -226,8 +214,7 @@ socket.addEventListener("error", (error) =>
 );
 
 const playSound = (sound) => {
-    console.log("playSound", sound);
     elements[sound].play();
 };
 
-focusInput(elements.nameInput);
+focusInput(nameInput);
